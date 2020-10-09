8a30935e99cf31edb3c959aa0e7bb5de5e341b8f
avinash kumar <avinash.sn38@gmail.com>
2020-10-09 13:47:21 +0530

    added User

--- demo/src/main/java/com/avinash/DemoApplication.java
+++ demo/src/main/java/com/avinash/DemoApplication.java
@@ -3,12 +3,16 @@ package com.avinash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.avinash.bean.User;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("firstApp");
+		User.info();
+		System.out.println("end of firstApp");
 	}
 
 }
---
+++ demo/src/main/java/com/avinash/bean/User.java
@@ -0,0 +1,10 @@
+package com.avinash.bean;
+
+public class User 
+{
+
+	public static void info()
+	{
+		System.out.println("this is avinash.");
+	}
+}
